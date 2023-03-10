a7190e3cdd51899c96b64556dd298fd3a39db31d
andrey.makurin <andrey.makurin@global-fintech.com>
2023-03-10 22:13:49 +0200

    homework_flask_compleate

--- app.py
+++ app.py
@@ -3,43 +3,63 @@ from flask import Flask, redirect, url_for, render_template, request, flash
 
 app = Flask(__name__)
 app.secret_key = 'my_secret_key'
-FILE = "memory/data.json"
+FILE = "memory_file/data.json"
+REGISTRATION_PAGE = "registration.html"
+LOGIN_PAGE = "login.html"
+STYLE = "style.css"
 
 
 def save_credentials(email, password):
     with open(FILE, "r") as infile:
         data = json.load(infile)
         if email in data:
-            print("Email already exist")
             return False
-        else:
-            data[email] = password
-            with open(FILE, "w") as outfile:
-                json.dump(data, outfile)
-                print("Email and password is saved")
-                return True
+        data[email] = password
+        with open(FILE, "w") as outfile:
+            json.dump(data, outfile)
+            return True
+
+
+def check_password(email, password):
+    with open(FILE, "r") as infile:
+        data = json.load(infile)
+        return email in data and data[email] == password
 
 
 @app.route("/registration", methods=['GET', 'POST'])
 def registration():
+    url_for('static', filename=STYLE)
     if request.method == 'GET':
-        return render_template('registration.html')
-    if request.method == 'POST':
+        return render_template(REGISTRATION_PAGE)
+    elif request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
         confirm_password = request.form.get('confirm_password')
         if password != confirm_password:
-            flash('Check your passwords')
-            save_credentials(email, password)
-            return render_template('registration.html')
+            flash('Password and confirm password does not match')
+            return render_template(REGISTRATION_PAGE)
+        else:
+            result = save_credentials(email, password)
+            if result is False:
+                flash('This user already exist')
+                return redirect(url_for('login'), code=302)
+            else:
+                flash('You are successfully registered')
+                return redirect(url_for('login'), code=302)
 
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-    email = request.form.get('email')
-    password = request.form.get('password')
-    if request.method == 'POST':
+    url_for('static', filename=STYLE)
+    if request.method == 'GET':
+        return render_template(LOGIN_PAGE)
+    elif request.method == 'POST':
+        email = request.form.get('email')
         password = request.form.get('password')
-        if password != 'string':
-            return redirect(url_for('registration'), code=302)
-    return render_template('login.html')
+        result = check_password(email, password)
+        if result is True:
+            flash('You are logged in')
+            return render_template(LOGIN_PAGE)
+        else:
+            flash('Your email or password is incorrect')
+            return render_template(LOGIN_PAGE)
